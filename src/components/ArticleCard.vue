592a51f2446bbe40f2e15bb5ea58b37f1baa88d3
Samar <samarbadriddinov.tv@gmail.com>
2022-11-03 14:34:50 +0200

    #45 artile card show

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="col">
+		<div class="card shadow-sm">
+			<svg
+				class="bd-placeholder-img card-img-top"
+				width="100%"
+				height="225"
+				xmlns="http://www.w3.org/2000/svg"
+				role="img"
+				aria-label="Placeholder: Thumbnail"
+				preserveAspectRatio="xMidYMid slice"
+				focusable="false"
+			>
+				<title>Placeholder</title>
+				<rect width="100%" height="100%" fill="#55595c"></rect>
+			</svg>
+
+			<div class="card-body">
+				<p class="card-title fw-bold">
+					{{ article.title }}
+				</p>
+				<p class="card-text">
+					{{ article.body }}
+				</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<div class="btn-group">
+						<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+						<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+					</div>
+					<small class="text-muted">9 mins</small>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		article: {
+			type: Object,
+			required: true,
+		},
+	},
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <template>
-	<div>
-		<h1>Home</h1>
+	<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+		<AriticleCard v-for="article in data" :article="article" :key="article.id" />
 	</div>
 </template>
 
 <script>
 import {mapState} from 'vuex'
+import AriticleCard from '@/components/ArticleCard.vue'
 export default {
+	components: {
+		AriticleCard,
+	},
 	computed: {
 		...mapState({
 			data: state => state.articles.data,
