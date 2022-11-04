90ebe13fd95b23d81daf24418da33a45da244f7d
Samar <samarbadriddinov.tv@gmail.com>
2022-11-04 07:42:51 +0200

    #50 artcilde detail ui

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,6 +1,40 @@
 <template>
 	<div v-if="article">
-		<h1>{{ article.title }}</h1>
+		<div class="row">
+			<div class="col-12">
+				<p class="fs-2">{{ article.title }}</p>
+			</div>
+			<div class="col-md-12 col-lg-8 mt-3">
+				<p class="opacity-75">Description:</p>
+				<p class="font-monospace">{{ article.description }}</p>
+			</div>
+			<div class="col-6 col-lg-2 mt-3">
+				<p class="opacity-75">Created at:</p>
+				<p class="font-monospace">{{ new Date(article.createdAt).toLocaleDateString('us') }}</p>
+			</div>
+			<div class="col-6 col-lg-2 mt-3">
+				<p class="opacity-75">Updated at:</p>
+				<p class="font-monospace">{{ new Date(article.updatedAt).toLocaleDateString('us') }}</p>
+			</div>
+			<div class="col-lg-4 mt-3">
+				<div class="d-flex align-items-center gap-3">
+					<div
+						class="d-flex justify-content-center align-items-center bg-primary"
+						style="width: 50px; height: 50px; border-radius: 100%"
+					>
+						<span class="text-light text-uppercase fw-bold">{{ article.author.username[0] }}</span>
+					</div>
+					<div class="fw-bold">
+						{{ article.author.username }}
+					</div>
+				</div>
+				<p class="font-monospace mt-2">{{ article.author.bio }}</p>
+			</div>
+			<div class="col-lg-8 mt-3">
+				<p class="opacity-75">Body:</p>
+				<p class="font-monospace">{{ article.body }}</p>
+			</div>
+		</div>
 	</div>
 </template>
 
