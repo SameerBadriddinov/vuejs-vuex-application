dde879c0203481a617c980b3e65efc2438acc814
Samar <samarbadriddinov.tv@gmail.com>
2022-11-04 07:16:41 +0200

    #49 article detail component

---
+++ src/components/ArticleDetail.vue
@@ -0,0 +1,18 @@
+<template>
+	<div v-if="article">
+		<h1>{{ article.title }}</h1>
+	</div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+	computed: {
+		...mapState({
+			article: state => state.articles.articleDetail,
+		}),
+	},
+}
+</script>
+
+<style></style>
--- src/views/ArticleDetailView.vue
+++ src/views/ArticleDetailView.vue
@@ -1,9 +1,22 @@
 <template>
-	<h1>Article details</h1>
+	<div>
+		<Loader v-if="isLoading" class="offset-md-6" />
+		<ArticleDetail v-else />
+	</div>
 </template>
 
 <script>
+import ArticleDetail from '@/components/ArticleDetail.vue'
+import {mapState} from 'vuex'
 export default {
+	components: {
+		ArticleDetail,
+	},
+	computed: {
+		...mapState({
+			isLoading: state => state.articles.isLoading,
+		}),
+	},
 	mounted() {
 		this.$store.dispatch('articleDetail', this.$route.params.slug)
 	},
